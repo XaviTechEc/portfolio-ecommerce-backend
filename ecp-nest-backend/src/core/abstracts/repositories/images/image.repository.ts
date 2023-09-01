64a3006d5900f8b9962e65fa08818089079f2dd3
XaviTechEc <sx.guzman@hotmail.com>
2023-09-01 15:30:28 -0500

    add: address folder resolvers

--- ecp-nest-backend/src/core/abstracts/repositories/images/image.repository.ts
+++ ecp-nest-backend/src/core/abstracts/repositories/images/image.repository.ts
@@ -1,5 +1,5 @@
 export abstract class IImageRepository<T> {
-  abstract getImagesBy(): Promise<T[]>;
+  abstract getImagesBy(fields: Partial<T>): Promise<T[]>;
   abstract getImageById(id: string): Promise<T>;
   abstract createImage(data: T): Promise<T>;
   abstract updateImage(id: string, data: T): Promise<T>;
---
+++ ecp-nest-backend/src/interface-adapters/resolvers/addresses/country.resolver.ts
@@ -0,0 +1,51 @@
+import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateCountryInput,
+  PaginationArgs,
+  SearchArgs,
+  UpdateCountryInput,
+} from 'src/core/dtos';
+import { ICountry } from 'src/core/entities';
+import { CountryType } from 'src/core/object-types';
+import { CountryUseCases } from 'src/use-cases';
+import { ParseUUIDPipe } from '@nestjs/common';
+
+@Resolver(() => CountryType)
+export class CountryResolver {
+  constructor(private countryUseCases: CountryUseCases) {}
+
+  @Query(() => [CountryType], { name: 'countries' })
+  getAllCountries(
+    @Args() paginationArgs: PaginationArgs,
+    @Args() searchArgs: SearchArgs<ICountry>,
+  ): Promise<ICountry[]> {
+    return this.countryUseCases.getAllCountries({ paginationArgs, searchArgs });
+  }
+
+  @Query(() => CountryType, { name: 'country' })
+  getCountryById(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+  ): Promise<ICountry> {
+    return this.countryUseCases.getCountryById(id);
+  }
+
+  @Mutation(() => CountryType)
+  createCountry(
+    @Args() createCountryInput: CreateCountryInput,
+  ): Promise<ICountry> {
+    return this.countryUseCases.createCountry(createCountryInput);
+  }
+
+  @Mutation(() => CountryType)
+  updateCountry(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @Args() updateCountryInput: UpdateCountryInput,
+  ): Promise<ICountry> {
+    return this.countryUseCases.updateCountry(id, updateCountryInput);
+  }
+
+  @Mutation(() => CountryType)
+  removeCountry(id: string): Promise<ICountry> {
+    return this.countryUseCases.removeCountry(id);
+  }
+}
